c895cf76043b43118a4c113db2ade6070c19f41b
Darya Syomina <d.semina10@gmail.com>
2023-03-23 12:41:27 +0300

    add price sum

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -4,12 +4,13 @@ import { Card } from '@components/Card';
 import gridStyles from '@pages/Products/Products.module.scss';
 import cartStore from '@store/CartStore';
 import { ProductModel } from '@store/models/Product';
+import { observer } from 'mobx-react-lite';
 import { useNavigate } from 'react-router-dom';
 
 import styles from './Cart.module.scss';
 
-const Cart = () => {
-  const { cartProducts } = cartStore;
+const Cart = observer(() => {
+  const { cartProducts, deleteFromCart } = cartStore;
 
   const navigate = useNavigate();
 
@@ -17,7 +18,11 @@ const Cart = () => {
     <div className={styles.page}>
       <div className={styles.content}>
         <div className={styles.header}>
-          <h1 className={styles.header__title}>My products</h1>
+          <h1 className={styles.header__title}>
+            My products
+            {cartProducts.length > 0 &&
+              `: $${cartProducts.reduce((acc, product) => acc + +product.content, 0)}`}
+          </h1>
         </div>
         {cartProducts.length > 0 ? (
           <div className={gridStyles.grid}>
@@ -39,6 +44,6 @@ const Cart = () => {
       </div>
     </div>
   );
-};
+});
 
 export default Cart;
